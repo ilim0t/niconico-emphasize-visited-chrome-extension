f0dae7cd83c2ebaddd82f3ef696546418c7dae38
ilim <ilim0t14@gmail.com>
2020-07-05 17:58:17 +0900

    Add full-screen improvements & Configure icons

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,3 +1,6 @@
+/* 
+    ランキングで、すでに訪れた動画ページの背景を暗くする。
+*/
 const videos = document.getElementsByClassName("RankingMainVideo");
 // const videos = document.getElementsByClassName("VideoItem");
 
@@ -10,4 +13,19 @@ Array.from(videos).forEach(async video => {
     if (visited.some(item => item)) {
         video.style.backgroundColor = "gainsboro";
     }
+});
+
+/* 
+    動画ページで、niconicoで実装されたフルスクリーンが本来のもの(ディスプレイ全画面に表示される)と異なっている問題を解消
+*/
+const observer = new MutationObserver(([mutation]) => {
+    if (mutation.target.classList.contains("is-fullscreen")) {
+        console.log("full");
+        mutation.target.requestFullscreen();
+    }
+});
+
+observer.observe(document.body, {
+    attributes: true,
+    attributeFilter: ["class"]
 });
